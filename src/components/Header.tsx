5f3fb4e21d99b609e3cd7c2c5e24f76916f11b62
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2024-12-15 22:04:30 +0100

    removed drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,16 @@
 // components/Header.tsx
 "use client";
 
-import MenuIcon from "@mui/icons-material/Menu";
-import HomeIcon from "@mui/icons-material/Home";
-import PeopleIcon from "@mui/icons-material/People";
-import WorkIcon from "@mui/icons-material/Work";
-import ContactsIcon from "@mui/icons-material/Contacts";
-import {
-  AppBar,
-  Box,
-  Drawer,
-  IconButton,
-  Link,
-  List,
-  ListItem,
-  ListItemText,
-  ListItemIcon,
-  Toolbar,
-} from "@mui/material";
+import { AppBar, Box, Link, Toolbar } from "@mui/material";
 import Image from "next/image";
 import NextLink from "next/link";
 import { useState } from "react";
 
 const navLinks = [
-  { label: "Home", href: "/", icon: <HomeIcon /> },
-  { label: "Chi siamo", href: "/chi-siamo", icon: <PeopleIcon /> },
-  { label: "Servizi", href: "/servizi", icon: <WorkIcon /> },
-  { label: "Contatti", href: "/contatti", icon: <ContactsIcon /> },
+  { label: "Home", href: "/" },
+  { label: "Chi siamo", href: "/chi-siamo" },
+  { label: "Servizi", href: "/servizi" },
+  { label: "Contatti", href: "/contatti" },
 ];
 
 export default function Header() {
@@ -37,7 +21,12 @@ export default function Header() {
   };
 
   return (
-    <AppBar position="sticky" color="primary" elevation={0} sx={{ px: 6 }}>
+    <AppBar
+      position="sticky"
+      color="primary"
+      elevation={0}
+      sx={{ px: { xs: 2, md: 6 }, alignItems: { xs: "center", md: "normal" } }}
+    >
       <Toolbar variant="dense">
         {/* Logo */}
         <Box sx={{ flexGrow: 1, pt: 2 }}>
@@ -69,38 +58,29 @@ export default function Header() {
         </Box>
 
         {/* Burger Menu for small screens */}
-        <Box sx={{ display: { xs: "block", md: "none" } }}>
-          <IconButton
-            color="inherit"
-            edge="end"
-            onClick={toggleDrawer(true)}
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Drawer
-            anchor="right"
-            open={drawerOpen}
-            onClose={toggleDrawer(false)}
-          >
-            <List component="nav" sx={{ width: 250 }}>
-              {navLinks.map((link) => (
-                <ListItem
-                  key={link.label}
-                  component={NextLink}
-                  href={link.href}
-                  onClick={toggleDrawer(false)}
-                >
-                  <>
-                    <ListItemIcon>{link.icon}</ListItemIcon>
-                    <ListItemText primary={link.label.toUpperCase()} />
-                  </>
-                </ListItem>
-              ))}
-            </List>
-          </Drawer>
-        </Box>
       </Toolbar>
+      <Box
+        sx={{
+          display: {
+            xs: "flex",
+            md: "none",
+            justifyContent: "center",
+          },
+        }}
+      >
+        {navLinks.map((link) => (
+          <Link
+            component={NextLink}
+            key={link.label}
+            href={link.href}
+            color="inherit"
+            underline="none"
+            sx={{ mx: 1, my: 2 }}
+          >
+            {link.label.toUpperCase()}
+          </Link>
+        ))}
+      </Box>
     </AppBar>
   );
 }
